cb7e6ba7af0c3a5346a4b4a8cc842a46d36a76ca
richelbilderbeek <richel@richelbilderbeek.nl>
2024-01-20 17:04:37 +0100

    Create CSVs

--- scripts/create_diamonds_tsv.sh
+++ scripts/create_diamonds_tsv.sh
@@ -4,6 +4,8 @@ rm -f diamonds.csv
 
 wget -q https://raw.githubusercontent.com/tidyverse/ggplot2/main/data-raw/diamonds.csv
 
+cp diamonds.csv ../data/diamonds_raw.csv
+
 sed -i 's/Very Good/very_good/g' diamonds.csv
 sed -i 's/Good/good/g' diamonds.csv
 sed -i 's/Fair/fair/g' diamonds.csv
@@ -11,6 +13,8 @@ sed -i 's/Premium/premium/g' diamonds.csv
 sed -i 's/Ideal/ideal/g' diamonds.csv
 
 tr -d '"' < diamonds.csv > diamonds_no_quotes.csv
+
+cp diamonds_no_quotes.csv ../data/diamonds_no_quotes.csv
 
 rm diamonds.csv
 
